2e5afd56d4df7a4ed96ee18704be8a846bcf0a83
KaterynaBachkalo <126788020+KaterynaBachkalo@users.noreply.github.com>
2024-03-26 15:06:43 +0000

    Deploying to gh-pages from @ KaterynaBachkalo/nanny-services@511f293d04a7238ebbb1436f8c997d2f015761a8 🚀

--- static/js/582.5d90af5e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkmy_app=self.webpackChunkmy_app||[]).push([[582],{582:(e,a,s)=>{s.r(a),s.d(a,{default:()=>c});s(43);var p=s(579);const c=()=>(0,p.jsx)("div",{children:"FavoritesPage"})}}]);
-//# sourceMappingURL=582.5d90af5e.chunk.js.map
